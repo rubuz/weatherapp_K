3ede65dfe7c3bdfd1e913e2adaf7ae80486057cf
rubuz <rubuz23@gmail.com>
2024-09-20 13:11:21 +0200

    updated the app, so the click on existing city update the card, and not add one

--- src/hooks/useCityManager.ts
+++ src/hooks/useCityManager.ts
@@ -16,10 +16,23 @@ const useCityManager = (
   // Add city to the list/array - check the length of the list, remove the last city if it exceeds 5
   const addCity = (weatherData: WeatherData) => {
     setCities((prevCities) => {
-      const newCities = [{ weatherData }, ...prevCities];
-      if (newCities.length > 5) {
-        newCities.pop();
+      const cityIndex = prevCities.findIndex(
+        (city) => city.weatherData.name === weatherData.name,
+      );
+
+      let newCities;
+      if (cityIndex !== -1) {
+        //Update existing city
+        newCities = [...prevCities];
+        newCities[cityIndex] = { weatherData };
+      } else {
+        // Add new city
+        newCities = [{ weatherData }, ...prevCities];
+        if (newCities.length > 5) {
+          newCities.pop();
+        }
       }
+
       // Save to local storage
       localStorage.setItem("cityHistory", JSON.stringify(newCities));
       return newCities;
